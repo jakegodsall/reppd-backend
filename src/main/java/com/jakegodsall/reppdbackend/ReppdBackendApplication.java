b40bd30669b0322564a9af521274ef4a6bf9f5b6
jakegodsall <jake.edward.godsall@gmail.com>
2023-11-28 15:02:26 +0000

    initial commit

---
+++ src/main/java/com/jakegodsall/reppdbackend/ReppdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.reppdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReppdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReppdBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jakegodsall/reppdbackend/ReppdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakegodsall.reppdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReppdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
