c8c22c8406bb2b816ec9a0564dbb078d1dbc21b6
jakegodsall <jake.edward.godsall@gmail.com>
2023-12-07 15:58:47 +0000

    added basic security configuration

---
+++ src/main/java/com/jakegodsall/reppdbackend/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.jakegodsall.reppdbackend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize ->
+                authorize.requestMatchers("/api/**").authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(15);
+    }
+}
